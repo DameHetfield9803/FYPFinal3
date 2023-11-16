be992d7b7c8c18e12a2f468f4a2526c9f22759af
damie <21011435@myrp.edu.sg>
2023-11-16 20:00:37 +0800

    changed home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,10 @@
 export default function Home() {
+    function getUser(){
+
+    }
     return (
         <div>
-            <h1>Home</h1>
-            <div>
-                <p>pending...</p>
-            </div>
+            <h1>Welcome </h1>
         </div>
     )
 }
