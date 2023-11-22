cf00c23c6bf7102f501abe820fd6501d917d3857
firdaus amin <21035358@myrp.edu.sg>
2023-11-22 16:45:55 +0800

    Fixed Error but bug remains for myprofile

--- src/components/AppraisalForm/AppraisalForm.jsx
+++ src/components/AppraisalForm/AppraisalForm.jsx
@@ -46,7 +46,23 @@ export default function App() {
   };
 
   return (
-    <div className="App">
+    <div>
+      <div className="topnav">
+        <a href="http://localhost:3000/Home" className="logo-link">
+          <img src="Assets/TSH.jpg" alt="Logo" width="310px" height="90px" />
+        </a>
+        <a href="http://localhost:3000/Dashboard">Dashboard</a>
+        <a href="http://localhost:3000/Attendance">Attendance</a>
+        <a href="http://localhost:3000/Accolades">Accolades</a>
+        <a href="http://localhost:3000/AppraisalForm">AppraisalForm</a>
+
+        {/* My Profile link */}
+        <Link to="/Profile" className="profile">
+          <img src="Assets/Profile-icon.jpg" alt="Profile Icon" width="30px" height="30px" />
+          <span>My Profile</span>
+        </Link>
+      </div>
+
       <h1>Enter the appraisal performance </h1>
 
       {inputList.length > 0 ? (
@@ -55,7 +71,7 @@ export default function App() {
             <input
               type="text"
               className="form-control"
-              placeholder={`Enter Appraisal Item`}
+              placeholder={`Input ${index + 1}`}
               value={input.input}
               onChange={(event) => handleInputChange(event, index)}
             />
