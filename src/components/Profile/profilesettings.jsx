6a95399aa7ca5cbcb694b6ece53093751792f8db
firdaus amin <21035358@myrp.edu.sg>
2023-12-13 16:19:02 +0800

    Updated Dashboard,Home,Psettings and reporterror

--- src/components/Profile/profilesettings.jsx
+++ src/components/Profile/profilesettings.jsx
@@ -1,50 +1,137 @@
-import React from 'react';
-import { auth } from "../../config/firebase";
-import { signOut } from "firebase/auth";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import './profilesettings.css';
+// Import the necessary modules.
+import React, { useState } from "react";
+import "./profilesettings.css";
 
-export default function ProfileSettings() {
-  const history = useHistory();
-
-  const logout = async () => {
-    try {
-      await signOut(auth);
-      history.push("/");
-    } catch (err) {
-      console.log(err);
-    }
+// Define the functional component ProfileSettings.
+const ProfileSettings = () => {
+  // Default profile information (cannot be changed).
+  const defaultProfileInfo = {
+    name: "User",
+    department: "BMX",
+    batchNumber: "1101",
   };
 
-  return (
+  // Default personal information (can be edited).
+  const defaultPersonalInfo = {
+    phone: "8888 8888",
+    email: "usertsh@tsh.com",
+  };
 
-    
+  // State variables for editable fields.
+  const [phone, setPhone] = useState(defaultPersonalInfo.phone);
+  const [email, setEmail] = useState(defaultPersonalInfo.email);
+
+  // State variable to track whether the fields are in edit mode.
+  const [isEditing, setIsEditing] = useState(false);
+
+  // Function to handle the submission of edited personal information.
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsEditing(false);
+    // Perform the necessary logic to save the edited personal information.
+    console.log("Edited Phone:", phone);
+    console.log("Edited Email:", email);
+  };
+
+  // JSX rendering the component.
+  return (
     <div>
-       {/* NAVBAR ITEMS */}  
-       <div className="topnav">
+      {/* Navbar Items */}
+      <div className="topnav">
+        {/* Logo */}
         <a href="/Home" className="logo-link">
           <img src="Assets/TSH.jpg" alt="Logo" width="310px" height="90px" />
         </a>
-            
-                <a href="/Dashboard">Dashboard</a>
-                <a href="/Attendance">Attendance</a>
-                <a href="/Accolades">Accolades</a>
-                <a href="/AppraisalForm">AppraisalForm</a>
 
-                
-         {/* My Profile link */}  
-         <a href="/Profile" className="profile">
+        {/* Navigation Links */}
+        <a href="/Dashboard">Dashboard</a>
+        <a href="/Attendance">Attendance</a>
+        <a href="/Accolades">Accolades</a>
+        <a href="/AppraisalForm">AppraisalForm</a>
+
+        {/* My Profile link*/}
+        <a href="/Profile" className="profile">
           <img src="Assets/Profile-icon.jpg" alt="Profile Icon" width="30px" height="30px" />
-        
-        <span>My Profile</span>
+          <span>My Profile</span>
         </a>
+      </div>
+      {/* End of Navbar Items */}
 
-            </div>
-            {/*END OF NAVBAR ITEMS */} 
-            
-      <h1>Profile Settings</h1>
-      {/* Include other JSX elements or components as needed */}
-      <button onClick={logout}>Logout</button>
+      {/* Profile Settings Section */}
+      <div className="profile-settings-container">
+        <h1>Profile Settings</h1>
+
+        {/* Display default profile information. */}
+        <div>
+          <p>
+            <strong>Name:</strong> {defaultProfileInfo.name}
+          </p>
+          <p>
+            <strong>Department:</strong> {defaultProfileInfo.department}
+          </p>
+          <p>
+            <strong>Batch Number:</strong> {defaultProfileInfo.batchNumber}
+          </p>
+        </div>
+
+        {/* Display editable personal information with edit buttons. */}
+        <div className="mb-3">
+          {/* Personal Phone Number */}
+          <p>
+            <strong>Personal Phone Number:</strong>{" "}
+            {isEditing ? (
+              <input
+                type="text"
+                className="form-control"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            ) : (
+              <span>{phone}</span>
+            )}
+            <button
+              className="btn btn-link"
+              onClick={() => setIsEditing(!isEditing)}
+            >
+              {isEditing ? "Cancel" : "Edit"}
+            </button>
+          </p>
+
+          {/* Personal Email */}
+          <p>
+            <strong>Personal Email:</strong>{" "}
+            {isEditing ? (
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            ) : (
+              <span>{email}</span>
+            )}
+            <button
+              className="btn btn-link"
+              onClick={() => setIsEditing(!isEditing)}
+            >
+              {isEditing ? "Cancel" : "Edit"}
+            </button>
+          </p>
+        </div>
+
+        {/* Form for saving changes. */}
+        {isEditing && (
+          <form onSubmit={handleSubmit}>
+            <button type="submit" className="btn btn-primary">
+              Save Changes
+            </button>
+          </form>
+        )}
+      </div>
+      {/* End of Profile Settings Section */}
     </div>
   );
 };
+
+// Export the ProfileSettings component as the default export.
+export default ProfileSettings;
--- src/components/ReportForm/reporterror.jsx
+++ src/components/ReportForm/reporterror.jsx
@@ -1,8 +1,119 @@
-export default function reporterror() {
-    return (
-        <div>
-            {/*Report Error Form,there is no need to link this to the database,an optional page to do */}
-            <h1> Is there a bug you have faced? Report it here and we will work on it as soon as possible! </h1>
+import React, { useState } from "react";
+
+// Define a functional component named ReportForm using React.
+const ReportForm = () => {
+  // Use the useState hook to create state variables for form data.
+  const [formData, setFormData] = useState({
+    name: "", // Initialize the name field as an empty string.
+    email: "", // Initialize the email field as an empty string.
+    description: "", // Initialize the description field as an empty string.
+  });
+
+  // Define a function to handle changes in the input fields.
+  const handleChange = (e) => {
+    // Extract the name and value from the event (e) object.
+    const { name, value } = e.target;
+    // Update the corresponding field in formData using the setFormData function.
+    setFormData({
+      ...formData, // Spread the existing formData to avoid losing other fields.
+      [name]: value, // Update the specific field with the new value.
+    });
+  };
+
+  // Define a function to handle form submission.
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior.
+    // Log the form data to the console (in a real application, you'd handle the data differently).
+    console.log("Form submitted:", formData);
+  };
+
+  // Return the JSX for rendering the form.
+  return (
+    <div>
+        {/* NAVBAR ITEMS */}
+      <div className="topnav">
+        <a href="/Home" className="logo-link">
+          <img src="Assets/TSH.jpg" alt="Logo" width="310px" height="90px" />
+        </a>
+
+        <a href="/Dashboard">Dashboard</a>
+        <a href="/Attendance">Attendance</a>
+        <a href="/Accolades">Accolades</a>
+        <a href="/AppraisalForm">AppraisalForm</a>
+
+
+        {/* My Profile link*/}
+        <a href="/Profile" className="profile">
+          <img src="Assets/Profile-icon.jpg" alt="Profile Icon" width="30px" height="30px" />
+
+          <span>My Profile</span>
+        </a>
+
+      </div>
+      {/*END OF NAVBAR ITEMS */}
+      <h1>Report Form</h1>
+
+      {/* Create a form element with an onSubmit event handler set to the handleSubmit function. */}
+      <form onSubmit={handleSubmit}>
+        {/* Input field for the user's name. */}
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name:
+          </label>
+          {/* Input element with type 'text', bound to the 'name' field in formData. */}
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange} // Set the onChange event handler to the handleChange function.
+            required // Mark the field as required.
+          />
         </div>
-    )
-}
+
+        {/* Input field for the user's email. */}
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          {/* Input element with type 'email', bound to the 'email' field in formData. */}
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange} // Set the onChange event handler to the handleChange function.
+            required // Mark the field as required.
+          />
+        </div>
+
+        {/* Textarea field for the report description. */}
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description:
+          </label>
+          {/* Textarea element bound to the 'description' field in formData. */}
+          <textarea
+            className="form-control"
+            id="description"
+            name="description"
+            rows="4"
+            value={formData.description}
+            onChange={handleChange} // Set the onChange event handler to the handleChange function.
+            required // Mark the field as required.
+          ></textarea>
+        </div>
+
+        {/* Submit button for the form. */}
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+// Export the ReportForm component as the default export.
+export default ReportForm;
