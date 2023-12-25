250e1bd03baf41e2a7ac6b38e4c38bfb0d54b874
21013885-LewEnQuan <106365837+21013885-LewEnQuan@users.noreply.github.com>
2023-12-25 23:05:51 +0800

    Added css for appraisal form

--- src/components/AppraisalForm/AppraisalForm.jsx
+++ src/components/AppraisalForm/AppraisalForm.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../NavBar/NavBar";
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import "./AppraisalForm.css"
 
 const AppraisalFormPage1 = ({ onNext, handleChange, data }) => {
   return (
