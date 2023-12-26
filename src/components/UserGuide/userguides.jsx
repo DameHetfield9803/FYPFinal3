fdb08b168c8abe392657cf10d0bb14eda3b66e83
firdaus amin <21035358@myrp.edu.sg>
2023-12-26 15:37:19 +0800

    Updated User Guide page with drop down

--- src/components/UserGuide/userguides.jsx
+++ src/components/UserGuide/userguides.jsx
@@ -1,47 +1,60 @@
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
+import Navbar from '../NavBar/NavBar';
+import './userguides.css';
+
+
+const VideoAccordion = ({ videos }) => {
+  const [openVideo, setOpenVideo] = useState(null);
+
+  const handleAccordionClick = (videoId) => {
+    setOpenVideo(videoId === openVideo ? null : videoId);
+  };
+
+  return (
+    <div className="video-accordion-container">
+      {videos.map((video) => (
+        <div key={video.id} className="accordion-item">
+          <div
+            className={`accordion-header ${video.id === openVideo ? 'open' : ''}`}
+            onClick={() => handleAccordionClick(video.id)}
+          >
+            {video.title}
+          </div>
+          {video.id === openVideo && (
+            <div className="accordion-content">
+              <p>{video.description}</p>
+              <ReactPlayer url={video.url} controls />
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default function UserGuides() {
-    return (
-        <div>
-            {/* NAVBAR ITEMS */}
-            <div className="topnav">
-                <a href="/Home" className="logo-link">
-                    <img src="Assets/TSH.jpg" alt="Logo" width="310px" height="90px" />
-                </a>
+  const videoOptions = [
+    { id: 1, title: '1.) How To Edit User Profile', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', description: 'Login > Homepage > My Profile > Edit > Save Changes' },
+    { id: 2, title: '2.) How To Provide Peer and Self Evaluation', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', description: 'Login > Homepage > Peer/Self Evaluation > Input Choices "Strongly Agree - Strongly Disagree" > Save Changes' },
+    { id: 3, title: '3.) How to Provide Manager Feedback', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', description: 'Login > Homepage > Manager Feedback > Edit > Input Choices "Strongly Agree - Strongly Disagree" > Save Changes' },
+    { id: 4, title: '4.) Video Guide 4', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', },
+    { id: 5, title: '5.) Video Guide 5', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', },
+    { id: 6, title: '6.) Video Guide 6', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', },
+    { id: 7, title: '7.) Video Guide 7', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', },
+    { id: 8, title: '8.) Video Guide 8', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', },
+    { id: 9, title: '9.) Video Guide 9', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', },
+    { id: 10, title:'10.) Video Guide 10', url: 'https://www.youtube.com/watch?v=9bZkp7q19f0', },
+  ];
 
-                <a href="/Attendance">Attendance</a>
-                <a href="/Accolades">Accolades</a>
-                <a href="/AppraisalForm">AppraisalForm</a>
-
-
-                {/* My Profile link */}
-                <a href="/Profile" className="profile">
-                    <img src="Assets/Profile-icon.jpg" alt="Profile Icon" width="30px" height="30px" />
-
-                    <span>My Profile</span>
-                </a>
-
-            </div>
-            {/*END OF NAVBAR ITEMS */}
-            {/*Start Of Embedded Video Items */}
-            <h2> User Guide</h2>
-            <p>Video Guide Example 1 (How to edit your profile)</p>
-            <ReactPlayer
-                url="https://www.youtube.com/watch?v=lHpYyYtkmrw"
-                controls
-                width="400"
-                height="400"
-            />
-            <p>Video Guide Example 2 (How to provide peer evaluation)</p>
-            <ReactPlayer
-                url="https://www.youtube.com/watch?v=lHpYyYtkmrw"
-                controls
-                width="200"
-                height="200"
-            />
-            {/*End Of Embedded Video Items */}
-
-        </div>
-
-    );
+  return (
+    <div>
+        <Navbar />
+    <div className="user-guides-container">
+         
+      <h2>Quick User Guide</h2>
+      <VideoAccordion videos={videoOptions} />
+    </div>
+    </div>
+  );
 }
