17896fa8f29ac211bda5a7437be1d744d15901a0
damie <21011435@myrp.edu.sg>
2024-01-02 17:11:03 +0800

    Made amendments to the App.js

---
+++ src/components/MCHandling/MCHandling.jsx
@@ -0,0 +1,17 @@
+export default function MCHandling()
+{
+    return(
+        <div>
+            <h1>MC Handling for TSH</h1>
+            <body>
+                <div>
+                    <form>
+                        {/* Upload the MC using an input type submission*/}
+                        <p>Upload your MC here</p>
+                        <input type="file">Submit Here</input>
+                    </form>
+                </div>
+            </body>
+        </div>
+    );
+}
