1d2544c48a41c9fbe14f0f447d4fb1cc0a71ff0e
21013885-LewEnQuan <21013885@myrp.edu.sg>
2024-01-10 03:23:56 +0800

    updated

--- backend/server.js
+++ backend/server.js
@@ -40,42 +40,30 @@ db.query(`SELECT * FROM employee`, (err, result) => {
   }
   return console.log(result);
 });
-// // Test database connection
-// app.get("/testDBConnection", (req, res) => {
-//   db.query("SELECT 1", (err, result) => {
-//     if (err) {
-//       console.error("Error connecting to the database:", err);
-//       return res
-//         .status(500)
-//         .json({ message: "Error connecting to the database" });
-//     }
-//     return res.status(200).json({ message: "Database connection successful" });
-//   });
-// });
 
-// // Manager feedback
-// // Route to handle submission of manager feedback
-// app.post("/submitManagerFeedback", (req, res) => {
-//   const { feedback, employeeId } = req.body;
+// Manager feedback
+// Route to handle submission of manager feedback
+app.post("/submitManagerFeedback", (req, res) => {
+  const { feedback, employeeId } = req.body;
 
-//   // Check if required data is provided
-//   if (!feedback || !employeeId) {
-//     return res
-//       .status(400)
-//       .json({ message: "Feedback and employee ID are required" });
-//   }
+  // Check if required data is provided
+  if (!feedback || !employeeId) {
+    return res
+      .status(400)
+      .json({ message: "Feedback and employee ID are required" });
+  }
 
-//   // Insert the feedback into the database
-//   const sql =
-//     "INSERT INTO manager_feedback (employee_id, feedback) VALUES (?, ?)";
-//   db.query(sql, [employeeId, feedback], (err, result) => {
-//     if (err) {
-//       console.error("Error submitting manager feedback:", err);
-//       return res.status(500).json({ message: "Error submitting feedback" });
-//     }
-//     return res.status(200).json({ message: "Feedback submitted successfully" });
-//   });
-// });
+  // Insert the feedback into the database
+  const sql =
+    "INSERT INTO manager_feedback (employee_id, feedback) VALUES (?, ?)";
+  db.query(sql, [employeeId, feedback], (err, result) => {
+    if (err) {
+      console.error("Error submitting manager feedback:", err);
+      return res.status(500).json({ message: "Error submitting feedback" });
+    }
+    return res.status(200).json({ message: "Feedback submitted successfully" });
+  });
+});
 
 app.listen(8081, () => {
   console.log("listening on port 8081...");
