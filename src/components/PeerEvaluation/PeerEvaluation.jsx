b6d0f2146e315a196ca100ad478db74454646ed8
21013885-LewEnQuan <106365837+21013885-LewEnQuan@users.noreply.github.com>
2024-01-09 18:26:51 +0800

    added form element

--- src/components/PeerEvaluation/PeerEvaluation.jsx
+++ src/components/PeerEvaluation/PeerEvaluation.jsx
@@ -32,9 +32,8 @@ export default function PeerEvaluation() {
   }
 
   return (
-
-    <form method='post' action='127.0.0.1:3306'>
     <div className="App">
+          <form method='post' action='127.0.0.1:3306'>
       <div className="container">
         <h1 className="mt-3">Peer Evaluation</h1>
        <b> <p className="mt-3">(Lowest Rating: 1, Neutral : 3, Highest Rating: 5)</p></b>
@@ -178,9 +177,8 @@ export default function PeerEvaluation() {
           </div>
         )}
       </div>
-      
+      </form>
    </div>
-   </form>
     
   );
 }
