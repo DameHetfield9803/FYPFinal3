c037f87c34afaed28c3535552602fc6b3330c5ce
21013885-LewEnQuan <21013885@myrp.edu.sg>
2024-01-10 03:03:30 +0800

    updare

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 
 const db = mysql.createConnection({
   // host is using mysql and a portable localhost
