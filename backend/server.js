61b168d45adfecb81e557a4b56200dfa90000c8f
damie <21011435@myrp.edu.sg>
2024-01-10 16:33:35 +0800

    nigeria

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +24,7 @@ const db = mysql.createConnection({
 
 db.connect((err => {
   if (err) throw err;
-  console.log('MySQL Connected');
+  console.log('MySQL successfully Connected...');
 }));
 
 // example of getting something from somewhere taking parameters request and response
@@ -38,8 +39,17 @@ db.connect((err => {
     });
 });*/
 
-app.get("/", (re, res) => {
-  return res.json("From Server.js...");
+app.get("/departments", function(re, res){
+  // querying from mydb.department
+  db.query("SELECT * FROM department;", (error, results) => {
+    // throws an error if there are errors connecting to either the database or the API
+    if (error) {
+      // Handle database errors appropriately
+      return res.status(500).json({ error: error.message });
+    }
+    // returns data as Stringified JSON if no errors are thrown
+    res.json({ departments: JSON.parse(JSON.stringify(results)) });
+  });
 });
 
 // Get employee records
@@ -50,19 +60,47 @@ app.get("/", (re, res) => {
 //   return console.log(result);
 // });
 
+app.get(`/`, (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
 async function submitPeerFeedback(date, desc,peerId, formId, staff_id){
   const res = await db.query(`INSERT INTO EMPLOYEE (date, feedback_text, peer_feedback_id, peer_id, staff_id), ${date}, ${desc}, ${peerId}, ${formId}, ${staff_id}`);
+  // res is saying that its inserting into employees using those fields, this is all JQuery.
   let msg = "Error in inserting data into database. \n";
   if(res.affectedRows){
       msg = `Successfully added.\n`
   }
   return {msg};
+
+  app.post()
 }
+
+async function getPeerFeedback()
+{
+  const res = await db.query(`SELECT * FROM peerfeedback;`);
+  if(res.affectedRows >=1){
+    return console.log("Successfully retrieved from database. \n");
+  }
+  else{
+    return err;
+  }
+}
+
+app.post('/submitpeerfeedback', async function(req,res,next){
+  try{
+    res.json();
+  }
+  catch (err){
+    console.error("There may be something wrong with the database or API. \n");
+    next(err);
+  }
+})
 
 // Manager feedback
 // Route to handle submission of manager feedback
-app.post("/submitManagerFeedback", (req, res) => {
-  console.log("Received feedback:", req.body); // Check if the request body is properly parsed
+app.post("/submitmanagerfeedback", (req, res) => {
+  console.log("Feedback received : ", req.body); // Check if the request body is properly parsed
   const { feedback, employeeId } = req.body;
 
   // Check if required data is provided
@@ -84,6 +122,6 @@ app.post("/submitManagerFeedback", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000...");
+app.listen(3001, () => {
+  console.log("listening on port 3001...");
 });
