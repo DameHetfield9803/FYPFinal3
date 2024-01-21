e78033dccaee37b1fd332b8dd20a9947ebbacfd4
21013885-LewEnQuan <21013885@myrp.edu.sg>
2024-01-21 15:30:01 +0800

    created Navbar component managerfeedback

---
+++ src/components/ManagerFeedback/NavHeader.jsx
@@ -0,0 +1,28 @@
+export function NavBar() {
+  return (
+    <>
+      {/* NAVBAR ITEMS */}
+      <div className="topnav">
+        <a href="/Home" className="logo-link">
+          <img src="Assets/TSH.jpg" alt="Logo" width="310px" height="80px" />
+        </a>
+        <a href="/Attendance">Attendance</a>
+        <a href="/Accolades">Accolades</a>
+        <a href="/AppraisalForm">AppraisalForm</a>
+
+        {/* My Profile link*/}
+        <a href="/Profile" className="profile">
+          <img
+            src="Assets/Profile-icon.jpg"
+            alt="Profile Icon"
+            width="30px"
+            height="30px"
+          />
+
+          <span>My Profile</span>
+        </a>
+      </div>
+      {/*END OF NAVBAR ITEMS */}
+    </>
+  );
+}
