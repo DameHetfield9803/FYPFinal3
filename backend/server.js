42c73e1a3868eb26db4f172a39fcae15857462e6
21013885-LewEnQuan <106365837+21013885-LewEnQuan@users.noreply.github.com>
2024-01-24 13:16:29 +0800

    someone forgot to put arrow function

--- backend/server.js
+++ backend/server.js
@@ -422,37 +422,36 @@ app.delete("/accolade", (req, res) => {
 
 // CRUD employee.job_role
 
-app.get("/getempjobrole", (req,res) => {
-  db.query("SELECT * FROM employee.job_role ;",(err, data) => {
-    if(err) return res.json(err);
+app.get("/getempjobrole", (req, res) => {
+  db.query("SELECT * FROM employee.job_role ;", (err, data) => {
+    if (err) return res.json(err);
     return res.json(data);
   });
 }); // TODO Firdaus
 
-app.post("/createempjobrole", (req,res) => {
+app.post("/createempjobrole", (req, res) => {
   const vals = [];
-  db.query("INSERT INTO WHERE ;", vals,(err,data) => {
-    if(err) return res.json(err);
+  db.query("INSERT INTO WHERE ;", vals, (err, data) => {
+    if (err) return res.json(err);
     return res.json(data);
   });
-}) // TODO Daniel
+}); // TODO Daniel
 
-app.put("/updateemployeejobrole", (req,res) => {
+app.put("/updateemployeejobrole", (req, res) => {
   const vals = [];
-  db.query("SET VALUES WHERE;", vals, (err,data){
-    if(err) return res.json(err);
+  db.query("SET VALUES WHERE;", vals, (err, data) => {
+    if (err) return res.json(err);
     return res.json(data);
   });
-}) // TODO Firdaus
+}); // TODO Firdaus
 
-
-app.delete("/deleteemployeejobrole", (req,res) => {
-  const vals = []; 
-  db.query("DELETE FROM WHERE ;", vals, (err,data) => {
-    if(err) return res.json(err);
+app.delete("/deleteemployeejobrole", (req, res) => {
+  const vals = [];
+  db.query("DELETE FROM WHERE ;", vals, (err, data) => {
+    if (err) return res.json(err);
     return res.json(data);
   });
-}) // TODO Daniel
+}); // TODO Daniel
 
 //---------------------------END OF CRUD---------------------------
 
