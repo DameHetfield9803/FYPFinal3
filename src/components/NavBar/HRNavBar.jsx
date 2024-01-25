ad63bc4aefd58d92d2e479b26a6a72b93c9ec8e2
21013885-LewEnQuan <106365837+21013885-LewEnQuan@users.noreply.github.com>
2024-01-25 22:45:31 +0800

    added navbar for HR only

---
+++ src/components/NavBar/HRNavBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./NavBar.css";
+
+export default function Navbar() {
+  return (
+    <div>
+      {/* Navbar Items */}
+      <div className="topnav">
+        {/* Logo */}
+        <a href="/Home" className="logo-link">
+          <img src="Assets/TSH.jpg" alt="Logo" width="310px" height="90px" />
+        </a>
+
+        {/* Navigation Links */}
+        <a href="/Attendance">Attendance</a>
+        <a href="/Accolades">Accolades</a>
+        <a href="/AppraisalForm">Appraisal Form</a>
+
+        {/* My Profile link*/}
+        <a href="/Profile" className="profile">
+          <img
+            src="Assets/Profile-icon.jpg"
+            alt="Profile Icon"
+            width="30px"
+            height="30px"
+          />
+          <span>My Profile</span>
+        </a>
+      </div>
+      {/* End of Navbar Items */}
+    </div>
+  );
+}
