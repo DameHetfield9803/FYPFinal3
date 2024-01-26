d995968bfab1b5a87c563e2555a3895de2c03117
LEE JIA AN DANIEL <21008277@myrp.edu.sg>
2024-01-26 17:24:06 +0800

    ice

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PeerEvaluation from "./components/PeerEvaluation/PeerEvaluation";
 import SelfEvaluation from "./components/SelfEvaluation/SelfEvaluation";
 import Attendance from "./components/Attendance/Attendance";
 import ManagerFeedback from "./components/ManagerFeedback/ManagerFeedback";
+import Accolades from "./components/Accolades/Accolades";
 import AppraisalForm from "./components/AppraisalForm/AppraisalForm";
 import Profile from "./components/Profile/profilesettings";
 import AppraisalItem from "./components/AppraisalItem/AppraisalItem";
@@ -21,17 +22,13 @@ import Home from "./components/Home/Home";
 import AttendanceSummary from "./components/Attendance/Attendancesummary";
 import EmailAutomate from "./components/Email/EmailAutomate";
 import ViewAccolades from "./components/Accolades/ViewAccolades";
-<<<<<<< HEAD
-import AddAccolade from "./components/Accolades/AddAccolade"; 
-import EmployeeNavBar from "./components/NavBar/EmployeeNavBar";
-
-=======
-//import AddAccolade from "./components/Accolades/AddAccolade";
 import ViewFeedbackList from "./components/ManagerFeedback/ManagerFeedbackList";
 import UpdateFeedback from "./components/ManagerFeedback/UpdateManagerFeedback";
 import ManagerFeedbackSuccess from "./components/ManagerFeedback/ManagerFeedbackSuccess";
 import PeerFeedbackList from "./components/PeerEvaluation/PeerEvalList";
-
+import ViewAccolades from "./components/Accolades/ViewAccolades";
+import AddAccolade from "./components/Accolades/AddAccolade"; 
+import EmployeeNavBar from "./components/NavBar/EmployeeNavBar";
 
 export default function App() {
   return (
@@ -46,24 +43,17 @@ export default function App() {
             <Home />
           </Route>
 
+          <Route exact path="/accolades">
+            <Accolades />
+          </Route>
+
           <Route exact path="/viewaccolades">
             <ViewAccolades />
           </Route>
 
-<<<<<<< HEAD
           <Route exact path="/addaccolades">
             <AddAccolade />
           </Route>
-          
-          <Route exact path="/EmpNavBar">
-            <EmployeeNavBar />
-          </Route>
-
-=======
-          {/*<Route exact path="/addaccolades">
-            <AddAccolade />
-          </Route>*/}
->>>>>>> 8c517c9ba88fae03664af8b4fe39e77fd2230307
 
           <Route exact path="/forgetpassword">
             <ForgetPassword />
@@ -134,6 +124,10 @@ export default function App() {
 
           <Route exact path="/Navbar">
             <Navbar />
+          </Route>
+
+          <Route exact path="/EmpNavBar">
+            <EmployeeNavBar />
           </Route>
 
           <Route exact path="/forbidden">
