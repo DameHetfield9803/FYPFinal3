64758e6cf4cb5d73f193589fd0ca4dba3440eb4f
firdaus amin <21035358@myrp.edu.sg>
2024-01-27 17:40:02 +0800

    Fixed CSS issue

--- src/components/Home/HRHomepage.jsx
+++ src/components/Home/HRHomepage.jsx
@@ -4,26 +4,26 @@ import "./HRHomepage.css";
 export default function HRHomepage() {
   return (
     <>
-      <h1>Human Resource Dashboard</h1>
-      <div className="HR-container">
-        <Link to="/Attendance">Attendance</Link> <br></br>
-        <Link to="/viewAccolades">Accolades</Link>
+      <h1 className="HR-homepage-h1">Human Resource Dashboard</h1>
+      <div className="HR-homepage-container">
+        <Link to="/Attendance" className="HR-homepage-link">Attendance</Link> <br></br>
+        <Link to="/viewAccolades" className="HR-homepage-link">Accolades</Link>
         <br></br>
-        <Link to="/AppraisalForm">AppraisalForm</Link>
+        <Link to="/AppraisalForm" className="HR-homepage-link">AppraisalForm</Link>
         <br></br>
-        <Link to="/AppraisalItem">AppraisalItem</Link>
+        <Link to="/AppraisalItem" className="HR-homepage-link">AppraisalItem</Link>
         <br></br>
-        <Link to="/Employee">Employee</Link>
+        <Link to="/Employee" className="HR-homepage-link">Employee</Link>
         <br></br>
-        <Link to="/PeerEvaluation">PeerEvaluation</Link>
+        <Link to="/PeerEvaluation" className="HR-homepage-link">PeerEvaluation</Link>
         <br></br>
-        <Link to="/SelfEvaluation">SelfEvaluation</Link>
+        <Link to="/SelfEvaluation" className="HR-homepage-link">SelfEvaluation</Link>
         <br></br>
-        <Link to="/ReportForm">ReportForm</Link>
+        <Link to="/ReportForm" className="HR-homepage-link">ReportForm</Link>
         <br></br>
-        <Link to="/ManagerFeedback">ManagerFeedback</Link>
+        <Link to="/ManagerFeedback" className="HR-homepage-link">ManagerFeedback</Link>
         <br></br>
-        <Link href="/userguides">UserGuide</Link>
+        <Link href="/userguides" className="HR-homepage-link">UserGuide</Link>
         <br></br>
       </div>
     </>
